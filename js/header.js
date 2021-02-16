183025072ffe339057369e83533f903bff776bf1
Bohdan Tkachuk <b.tkachuk26@gmail.com>
2021-02-16 10:29:03 +0200

    Header added some animations

--- js/header.js
+++ js/header.js
@@ -2,19 +2,19 @@ const toggleBar = document.querySelector('.toggle')
 const navExit = document.querySelector('.nav-exit')
 let menu = document.getElementsByTagName('article')[0]
 
-function opacity(x){
-    
-}
-
 toggleBar.addEventListener('click', () =>{
+    document.querySelector('.toggle').classList.add("rotate")
     menu.classList.add('open')
+    menu.classList.remove('hidden')
     setTimeout("document.querySelector('.content').style.opacity = 1", 500)
 })
 
 navExit.addEventListener('click', () => {
+    
     document.querySelector('.content').style.opacity = 0
     setTimeout("menu.classList.remove('open')",300)
-    
+    setTimeout("menu.classList.add('hidden')",300)
+    setTimeout("document.querySelector('.toggle').classList.remove('rotate')",500)
     
 })
 
